880f9fb28a5ee0b83a117fee6f237131e429c7d1
mosunov.nn <colt92@inbox.ru>
2023-03-14 10:13:23 +0500

    Initial commit

---
+++ src/main/java/me/project/animaltrackapp/AnimalTrackAppApplication.java
@@ -0,0 +1,13 @@
+package me.project.animaltrackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalTrackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalTrackAppApplication.class, args);
+	}
+
+}
